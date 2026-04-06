acd77ed0ed3e30891ed2fe7550ec3acb91d19e7a
theriteshnandan-collab <theriteshnandan@gmail.com>
2026-04-06 11:33:42 +0530

    Make base path dynamic for GitHub and Vercel compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/THEDENTIST/',
+  base: process.env.GITHUB_ACTIONS ? '/THEDENTIST/' : '/',
   plugins: [react(), tailwindcss()],
 })
